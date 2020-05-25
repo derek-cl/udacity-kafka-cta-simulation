c21a714bfdd44e9d1a4cb98b092c18e418cc64a5
Derek <derek-cl@users.noreply.github.com>
2020-05-25 15:42:55 +0100

    init

---
+++ consumers/.ipynb_checkpoints/faust_stream-checkpoint.py
@@ -0,0 +1,78 @@
+"""Defines trends calculations for stations"""
+import logging
+
+import faust
+from dataclasses import asdict, dataclass
+
+logger = logging.getLogger(__name__)
+
+
+# Faust will ingest records from Kafka in this format
+class Station(faust.Record):
+    stop_id: int
+    direction_id: str
+    stop_name: str
+    station_name: str
+    station_descriptive_name: str
+    station_id: int
+    order: int
+    red: bool
+    blue: bool
+    green: bool
+
+
+# Faust will produce records to Kafka in this format
+class TransformedStation(faust.Record):
+    station_id: int
+    station_name: str
+    order: int
+    line: str
+
+        
+# TODO: Define a Faust Stream that ingests data from the Kafka Connect stations topic and
+#   places it into a new topic with only the necessary information.
+app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
+# TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
+topic = app.topic("com.udacity.cta.stations", value_type=Station)
+# TODO: Define the output Kafka Topic
+out_topic = app.topic("com.udacity.cta.transformed-stations", partitions=1)
+# TODO: Define a Faust Table
+table = app.Table(
+   "transformedStations",
+   default=TransformedStation,
+   partitions=1,
+   changelog_topic=out_topic,
+)
+
+
+#
+#
+# TODO: Using Faust, transform input `Station` records into `TransformedStation` records. Note that
+# "line" is the color of the station. So if the `Station` record has the field `red` set to true,
+# then you would set the `line` of the `TransformedStation` record to the string `"red"`
+#
+#
+@app.agent(topic)
+async def stationevent(stations):
+    #
+    #  Group By station_id
+    #
+  
+    async for se in stations:
+
+        if se.red:
+            line = 'red'
+        elif se.green:
+            line='green'
+        elif se.blue:
+            line='blue'
+        else:
+            line='no-color'
+    
+        transformed_station = TransformedStation(se.station_id, se.station_name, se.order, line)
+        
+        table[se.station_id] = transformed_station
+
+
+if __name__ == "__main__":
+    app.main()
---
+++ producers/models/turnstile.py
@@ -0,0 +1,77 @@
+"""Creates a turnstile data producer"""
+import logging
+from pathlib import Path
+
+from confluent_kafka import avro
+
+from models.producer import Producer
+from models.turnstile_hardware import TurnstileHardware
+
+
+logger = logging.getLogger(__name__)
+
+
+class Turnstile(Producer):
+    key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
+
+    value_schema = avro.load(
+       f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
+    )
+
+    def __init__(self, station):
+        """Create the Turnstile"""
+        
+        print('init turnstile')
+        
+        station_name = (
+            station.name.lower()
+            .replace("/", "_and_")
+            .replace(" ", "_")
+            .replace("-", "_")
+            .replace("'", "")
+        )
+
+        #
+        #
+        # topic name, number of partitions, and number of
+        # replicas
+        #self.station.color.name"
+        #
+        topic_name = f"{Producer.TOPIC_PREFIX}.turnstiles"
+        super().__init__(
+            topic_name,
+            key_schema=Turnstile.key_schema,
+            value_schema=Turnstile.value_schema,
+            num_partitions=20,
+            num_replicas=1,
+        )
+        self.station = station
+        self.turnstile_hardware = TurnstileHardware(station)
+
+    def run(self, timestamp, time_step):
+        """Simulates riders entering through the turnstile."""
+        num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
+#         logger.info("turnstile kafka integration incomplete - skipping")
+        #
+        #
+        # emitting a message to the turnstile topic for the number
+        # of entries that were calculated
+        #
+        #
+
+        payload = {
+               "station_id":  self.station.station_id,
+               "station_name": self.station.name,
+                "line": self.station.color.name
+               
+           }
+            
+        self.producer.produce(
+            key_schema = Turnstile.key_schema,
+            value_schema = Turnstile.value_schema ,
+           topic=self.topic_name,
+           key={"timestamp": self.time_millis()},
+           value=payload
+        )
+        
+        
